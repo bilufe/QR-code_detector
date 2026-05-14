6de5531e2eedf6bd09ccdbd655b42dd3c318e7bd
Rodrigo Zimmermann <rodrigo.zimmermann23@gmail.com>
2026-05-13 21:47:25 -0300

    Add files via upload
    Versão inicial.
    Atenção: o aplicativo tem limitação de um QRCode por PDF.

---
+++ script.js
@@ -0,0 +1,50 @@
+const elementoExibidor = document.getElementById('elementoExibidor');
+const elFileName = document.getElementById('fileName');
+
+document.getElementById('seletorArquivo').addEventListener('change', async(e) => {
+    const file = e.target.files[0];
+    const arrayBuffer = await file.arrayBuffer();
+
+    const pdf = await pdfjsLib.getDocument({data: arrayBuffer}).promise;
+
+    for (let p = 1; p <= pdf.numPages; p++){
+        const page = await pdf.getPage(p);
+        const viewport = page.getViewport({scale: 2});
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+
+        await page.render({
+            canvasContext: ctx,
+            viewport
+        }).promise;
+
+        const imageData = ctx.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+        const code = jsQR(
+            imageData.data,
+            imageData.width,
+            imageData.height
+        );
+
+        if(code){
+            if(elementoExibidor.classList.contains('hidden')){
+                elementoExibidor.classList.remove('hidden');
+            };
+
+            elementoExibidor.href = code.data;
+            elementoExibidor.textContent = code.data
+
+            elFileName.textContent = `Arquivo carregado: ${file.name}`;
+
+            document.querySelector('form').reset();
+        };
+
+    };
+});
